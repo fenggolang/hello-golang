fac4eeed8fc4ab9a86f09006e29ab5b868a2fffc
wangpengcheng <wangpengcheng.sh@chinaccs.cn>
2019-10-22 17:22:52 +0800

    12

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"os"
 )
 
 type ContentData struct {
@@ -12,8 +11,6 @@ type ContentData struct {
 }
 
 func main() {
-	pwd, _ := os.Getwd()
-	fmt.Printf("当前路径:%v\n", pwd)
 	tmpl := template.Must(template.ParseFiles("./index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := ContentData{
